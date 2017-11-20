b6e8a41ce9fb405dad47f9550d040f023259570f
Rick <wrightricky64@gmail.com>
2017-11-20 12:40:36 +0000

    Made the program file more understandable
    This update makes the program.cs file more understandable to the non-coding public.
    
    This file will help them understand what this code is doing while it's being used.

--- src/KiraBot/Program.cs
+++ src/KiraBot/Program.cs
@@ -1,21 +1,31 @@
-﻿using System;
+//System Requirements
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
-using KiraBot.Services;
 using NLog;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Collections.Immutable;
+//Discord Requirements
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+//KiraBot Requirements
+using KiraBot;
+using KiraBot.Services;
+
+//This file is mandatory for KiraBot.
+//This file is the shard code initialization.
+//Without this file, this bot would not be operational with just a few files filled with C# code doing absolute fuck all.
+//Do not remove.
 
 namespace KiraBot
 {
     public class Program
     {
+	    	//Initializing the logger for the entire program.
 		private Logger _log;
 
 		public static uint OkColor { get; } = 0x00ff00;
@@ -23,17 +33,17 @@ namespace KiraBot
 
 		public static CommandService CommandService { get; private set; }
 		public static DiscordShardedClient Client { get; private set; }
-
+		//Initializing the bot sequence.
 		static void Main(string[] args)
             => new Program().Start().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
         private IConfiguration _config;
-
+		//Initializing bot tasks within Shard #1.
 		public async Task Start()
         {
 			_log = LogManager.GetCurrentClassLogger();
-
+		//Logging the startup.
 			_log.Info("Starting KiraBot!");
             _client = new DiscordSocketClient();
             _config = BuildConfig();
@@ -44,12 +54,12 @@ namespace KiraBot
 
             await _client.LoginAsync(TokenType.Bot, _config["Token"]);
             await _client.StartAsync();
-
+			//Initialization completed.
 			_log.Info("Initialization Completed.");
 
             await Task.Delay(-1);
 		}
-
+		//Setting the Bot's Shard to display this as the game within the Discord client.
 		public async Task SetGame()
 		{
 			await _client.SetGameAsync("with Wumpus!");
@@ -72,7 +82,7 @@ namespace KiraBot
                 // Add additional services here...
                 .BuildServiceProvider();
         }
-
+	//Grabbing the configuration json file and embedding it within the project.
         private IConfiguration BuildConfig()
         {
             return new ConfigurationBuilder()
@@ -82,3 +92,5 @@ namespace KiraBot
         }
 	}
 }
+
+//Basic initialization completed.
